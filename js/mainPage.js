1aeb55a1095060aee936b16431777e6aba658458
anscksh <moonchanho1203@naver.com>
2021-12-13 02:11:11 +0900

    1213_02:10

--- js/mainPage.js
+++ js/mainPage.js
@@ -7,10 +7,11 @@ function openMainTab(product, element) {
     }
     for (i = 0; i < tabButton.length; i++) {
         tabButton[i].style.backgroundColor = "";
+        tabButton[i].style.opacity = "0.8";
     }
     document.getElementById(product).style.display = "block";
-    element.style.backgroundColor = "lightgray";
-    element.style.opacity = "0.7";
+    element.style.backgroundColor = "darkslategray";
+    element.style.opacity = "1";
 }
 
 document.getElementById("default").click();
